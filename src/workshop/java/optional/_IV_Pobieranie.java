689f60f545f0cbe28a02fc6c35e547942196ce38
mpietrewicz <torsenawd99>
2019-07-24 11:10:08 +0200

    Wykorzystanie metod get, orElseGet, orElse, orElseThrow (z użyciem lambd)

--- src/workshop/java/optional/_IV_Pobieranie.java
+++ src/workshop/java/optional/_IV_Pobieranie.java
@@ -10,6 +10,7 @@ public class _IV_Pobieranie {
                    T get()
                    T orElse(T other)
                    T orElseGet(Supplier<? extends T> other)
+                   Wykorzystanie metod get, orElseGet, orElse, orElseThrow (z użyciem lambd)
 
                    <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier)
 
@@ -24,11 +25,31 @@ public class _IV_Pobieranie {
         System.out.println("Po wywołaniu orElseGet " + defaultText);
         defaultText = Optional.ofNullable(text).orElse(getDefaultValue());
         System.out.println("Po wywołaniu orElse " + defaultText);
+
+        System.out.println("---");
+
+        String name = null;
+        String myDefaultText = Optional.ofNullable(name).orElseGet(_IV_Pobieranie::getDefaultValue);
+        System.out.println("Po wywołaniu orElseGet " + myDefaultText);
+
+        myDefaultText = Optional.ofNullable(name).orElse(getDefaultValue());
+        System.out.println("Po wywołaniu orElse " + defaultText);
+
+        myDefaultText = Optional.ofNullable(name).orElseThrow(_IV_Pobieranie::getThrowValue);
+        myDefaultText = Optional.ofNullable(name).orElseThrow(() -> getThrowValue());
+        myDefaultText = Optional.ofNullable(name).orElseThrow(() -> new RuntimeException("___Mój_RuntimeException___"));
+
+
     }
 
     public static String getDefaultValue() {
         System.out.println("inside getDefaultValue");
         return "Default Value";
+    }
+
+    public static RuntimeException getThrowValue() {
+        System.out.println("<<< W przypadku null'a wyswietlam ten tekst i zwracam RuntimeException >>>");
+        return new RuntimeException("___Mój_RuntimeException___");
     }
 
 }
